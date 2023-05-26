e7f231b7a02735ee0a08ff1befebe88c8436aed0
256randombits <121695324+256randombits@users.noreply.github.com>
2023-05-27 02:22:33 +0300

    Add endpoint: amount of journeys starting from the station

---
+++ citybike-backend/sqitch-migrations/deploy/api_stats_station_departures.sql
@@ -0,0 +1,57 @@
+-- Deploy citybikes:api_stats_station_departures to pg
+-- requires: journeys
+-- requires: initialize_postgrest
+
+BEGIN;
+DO $$
+BEGIN
+
+EXECUTE FORMAT('
+  CREATE VIEW %I.stats_station_departures AS
+    SELECT
+        s_dep.id AS id,
+
+        COUNT(j.departure_station_id) AS amount
+
+    FROM internal.journeys j
+      RIGHT OUTER JOIN internal.stations s_dep ON j.departure_station_id = s_dep.id
+
+    GROUP BY s_dep.id;
+
+', utils.get_api_schema ());
+
+
+EXECUTE FORMAT('
+  CREATE VIEW %I.stats_monthly_station_departures AS
+    SELECT
+        s_dep.id AS id,
+
+        COUNT(j.departure_station_id) AS amount,
+
+        date_trunc(''month'', j.departure_time) month_timestamp
+
+    FROM internal.journeys j
+      RIGHT OUTER JOIN internal.stations s_dep ON j.departure_station_id = s_dep.id
+
+    GROUP BY s_dep.id, month_timestamp;
+
+', utils.get_api_schema ());
+
+-- Privileges
+
+EXECUTE FORMAT('
+  GRANT SELECT ON %I.stats_station_departures TO %I;
+', utils.get_api_schema (), utils.get_anon_role ());
+
+EXECUTE FORMAT('
+  GRANT SELECT ON %I.stats_monthly_station_departures TO %I;
+', utils.get_api_schema (), utils.get_anon_role ());
+
+NOTIFY pgrst,
+'reload schema';
+
+END
+$$
+LANGUAGE plpgsql;
+
+COMMIT;
---
+++ citybike-backend/sqitch-migrations/verify/api_stats_station_departures.sql
@@ -0,0 +1,7 @@
+-- Verify citybikes:api_stats_station_departures on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
