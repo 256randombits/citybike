af097f1dc1f61bd1701340a167f958777c3c5b5d
256randombits <121695324+256randombits@users.noreply.github.com>
2023-05-30 04:06:46 +0300

    Wrap all stats into 'stats'

--- citybike-backend/sqitch-migrations/deploy/api_stations_stats.sql
+++ citybike-backend/sqitch-migrations/deploy/api_stations_stats.sql
@@ -17,53 +17,34 @@ CREATE VIEW api.stats_departures_monthly AS
 CREATE VIEW api.stats_departures AS
   SELECT
     station_id,
-
     SUM(departures_count) AS departures_count,
-
     AVG(average_distance_in_meters) AS average_distance_in_meters
-
   FROM api.stats_departures_monthly
 
   GROUP BY station_id;
 
 CREATE VIEW api.journeys_from_station_to_station_monthly AS
   SELECT
-
     s_dep.id AS departure_station_id,
-
     s_ret.id AS return_station_id,
-
     COUNT(j.return_station_id) AS journeys_count,
-
     date_trunc('month', j.departure_time) month_timestamp
-
   FROM api.stations s_dep
-
     FULL OUTER JOIN api.journeys j
-
       ON j.departure_station_id = s_dep.id
-
     FULL OUTER JOIN api.stations s_ret 
-
       ON return_station_id = s_ret.id
-
   GROUP BY s_dep.id, s_ret.id, month_timestamp;
 
 CREATE VIEW api.journeys_from_station_to_station AS
   SELECT
-
     departure_station_id,
-
     return_station_id,
-
     SUM(journeys_count) AS journeys_count
-
   FROM api.journeys_from_station_to_station_monthly
-
   GROUP BY departure_station_id, return_station_id;
 
 CREATE VIEW api.top5_destinations_monthly AS
-
   SELECT DISTINCT ON (departure_station_id, month_timestamp) 
           departure_station_id,
           month_timestamp,
@@ -80,15 +61,13 @@ CREATE VIEW api.top5_destinations_monthly AS
     				            UNBOUNDED FOLLOWING);
 
 CREATE VIEW api.top5_destinations AS
-
-  SELECT DISTINCT ON (departure_station_id) departure_station_id,
-  
+  SELECT DISTINCT ON (departure_station_id)
+          departure_station_id,
     		  nth_value(return_station_id, 1) OVER w rank1, 
     		  nth_value(return_station_id, 2) OVER w rank2, 
     		  nth_value(return_station_id, 3) OVER w rank3, 
     		  nth_value(return_station_id, 4) OVER w rank4, 
     		  nth_value(return_station_id, 5) OVER w rank5
-  
     	  FROM api.journeys_from_station_to_station
     	  WINDOW w AS (PARTITION BY departure_station_id 
     				            ORDER BY journeys_count DESC
@@ -120,7 +99,6 @@ $$ LANGUAGE SQL;
 DO $$
 DECLARE
 BEGIN
-
 -- Computed/Virtual columns to put stations into the response.
 FOR counter in 1..5 LOOP
   EXECUTE FORMAT(
@@ -130,13 +108,48 @@ FOR counter in 1..5 LOOP
       $function$ STABLE LANGUAGE SQL;
     ', counter, counter);
 END loop;
+END$$;
+
+CREATE VIEW api.stats_station AS
+  SELECT 
+    sd.station_id AS station_id,
+    sd.departures_count AS departures_count,
+    sd.average_distance_in_meters AS average_distance_in_meters
+  FROM api.stats_departures sd;
+
+  CREATE FUNCTION api.stats(api.stations) RETURNS SETOF api.stats_station AS $function$
+    SELECT * FROM api.stats_station WHERE station_id = $1.id
+  $function$ STABLE LANGUAGE SQL;
+
+CREATE FUNCTION api.top5_destinations(api.stats_station)
+  RETURNS SETOF api.top5_destinations ROWS 1 AS $$
+    SELECT departure_station_id, rank1, rank2, rank3, rank4, rank5
+    FROM api.top5_destinations t5d
+    WHERE t5d.departure_station_id = $1.station_id
+$$ LANGUAGE SQL;
+
+DO $$
+DECLARE
+BEGIN
+-- Computed/Virtual columns to put stations into the response.
+FOR counter in 1..5 LOOP
+  EXECUTE FORMAT(
+    'CREATE FUNCTION api.rank%s_destination(api.top5_destinations) 
+      RETURNS SETOF api.stations ROWS 1 AS $function$
+        SELECT * FROM api.stations WHERE id = $1.rank%s
+      $function$ STABLE LANGUAGE SQL;
+    ', counter, counter);
+END loop;
+END$$;
+
 
 -- Permissions
 GRANT SELECT ON api.stats_station_monthly TO web_anon;
 GRANT SELECT ON api.top5_destinations_monthly TO web_anon;
+GRANT SELECT ON api.stats_station TO web_anon;
+GRANT SELECT ON api.top5_destinations TO web_anon;
 
 NOTIFY pgrst,
 'reload schema';
 
-END$$;
 COMMIT;
--- citybike-backend/sqitch-migrations/revert/api_stations_stats.sql
+++ citybike-backend/sqitch-migrations/revert/api_stations_stats.sql
@@ -6,6 +6,18 @@ DO $$
 BEGIN
 FOR counter in 1..5 LOOP
   EXECUTE FORMAT(
+    'DROP FUNCTION api.rank%s_destination(api.top5_destinations);
+    ', counter);
+END loop;
+END$$;
+
+DROP FUNCTION api.stats(api.stations);
+DROP FUNCTION api.top5_destinations(api.stats_station);
+DROP VIEW api.stats_station;
+DO $$
+BEGIN
+FOR counter in 1..5 LOOP
+  EXECUTE FORMAT(
     'DROP FUNCTION api.rank%s_destination(api.top5_destinations_monthly);
     ', counter);
 END loop;
